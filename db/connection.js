f3a5e497acc3b1cfd502bbd6f8f6cc8eef2ce495
rwaynewhite <78130055+rwaynewhite15@users.noreply.github.com>
2021-08-29 16:05:36 -0400

    added voter routes

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2");
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MYSQL username,
+    user: "root",
+    //Your MYSQL password
+    password: "",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+module.exports = db;
