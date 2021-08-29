12e3ca3dd3c2ca838f51301232e82aa3bec77364
rwaynewhite <78130055+rwaynewhite15@users.noreply.github.com>
2021-08-29 14:12:52 -0400

    use JOIN to combine data from two tables using foreign keys

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     // Your MYSQL username,
     user: "root",
     //Your MYSQL password
-    password: "RieceeFijiBob2532",
+    password: "",
     database: "election",
   },
   console.log("Connected to the election database.")
@@ -24,7 +24,11 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
@@ -39,7 +43,12 @@ app.get("/api/candidates", (req, res) => {
 
 // Get/Read a single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
