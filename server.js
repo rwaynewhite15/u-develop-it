03b3e58b8f69857243c26cf249ab3bdc10a8b0de
rwaynewhite <78130055+rwaynewhite15@users.noreply.github.com>
2021-08-29 00:49:26 -0400

    Ready for GET routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,48 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MYSQL username,
+        user: 'root',
+        //Your MYSQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+// // GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+// // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
